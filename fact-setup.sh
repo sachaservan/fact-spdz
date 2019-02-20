aafdf739e8d12eab25807fcdb5554c544ebf1a65
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2019-02-20 16:41:03 -0500

    add setup script

---
+++ fact-setup.sh
@@ -0,0 +1,50 @@
+# install necessary packages
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential
+sudo apt-get install libcrypto++6 libcrypto++6-dbg libcrypto++-dev
+sudo apt-get install python
+sudo apt-get install libgmp3-dev
+sudo apt-get install golang-go
+
+# config stuff
+mylocal="$HOME/local"
+mkdir -p ${mylocal}
+cd ${mylocal}
+
+# install MPIR 3.0.0
+curl -O "http://mpir.org/mpir-3.0.0.tar.bz2"
+tar xf mpir-3.0.0.tar.bz2
+cd mpir-3.0.0
+./configure --enable-cxx --prefix="${mylocal}/mpir"
+make && make check && make install
+
+# install OpenSSL 1.1.0
+cd $mylocal
+curl -O "https://www.openssl.org/source/openssl-1.1.0j.tar.gz"
+tar -xf openssl-1.1.0j.tar.gz
+cd openssl-1.1.0j
+./config --prefix="${mylocal}/openssl"
+make && make install
+
+
+# update bashrc
+echo "export mylocal=$HOME/local" >> $HOME/.bashrc
+echo "export mylocal=$HOME/local" >> $HOME/.bashrc
+echo "export mylocal=$HOME/local" >> $HOME/.bashrc
+echo "export mylocal=$HOME/local" >> $HOME/.bashrc
+
+# export OpenSSL paths
+echi "export PATH=\"${mylocal}/openssl/bin/:${PATH}\"" >> $HOME/.bashrc
+echo "export C_INCLUDE_PATH=${mylocal}/openssl/include/:${C_INCLUDE_PATH}\"" >> $HOME/.bashrc
+echo "export CPLUS_INCLUDE_PATH=${mylocal}/openssl/include/:${CPLUS_INCLUDE_PATH}\"" >> $HOME/.bashrc
+echo "export LIBRARY_PATH=${mylocal}/openssl/lib/:${LIBRARY_PATH}\"" >> $HOME/.bashrc
+echo "export LD_LIBRARY_PATH=${mylocal}/openssl/lib/:${LD_LIBRARY_PATH}\"" >> $HOME/.bashrc
+
+# export MPIR paths
+echo "export PATH=\"${mylocal}/mpir/bin/:${PATH}\"" >> $HOME/.bashrc
+echo "export C_INCLUDE_PATH=\"${mylocal}/mpir/include/:${C_INCLUDE_PATH}\"" >> $HOME/.bashrc
+echo "export CPLUS_INCLUDE_PATH=\"${mylocal}/mpir/include/:${CPLUS_INCLUDE_PATH}\"" >> $HOME/.bashrc
+echo "export LIBRARY_PATH=\"${mylocal}/mpir/lib/:${LIBRARY_PATH}\"" >> $HOME/.bashrc
+echo "export LD_LIBRARY_PATH=\"${mylocal}/mpir/lib/:${LD_LIBRARY_PATH}\"" >> $HOME/.bashrc
+
