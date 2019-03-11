70b526c078ad9e1e0f9a7bdcb244b991aae22ea6
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2019-03-11 08:34:30 -0400

    full tests

--- benchmark-online.py
+++ benchmark-online.py
@@ -67,7 +67,7 @@ for i in range(num_trials):
 	time_ttest_1000.append(time_elapsed)
 
 print("===================================")
-print("t-test 1000: " + str(time_ttest_1000))
+print("t-test 1,000: " + str(time_ttest_1000))
 print("===================================")
 
 for i in range(num_trials):
@@ -78,7 +78,7 @@ for i in range(num_trials):
 	time_pearson_1000.append(time_elapsed)
 
 print("===================================")
-print("pearson 1000: " + str(time_pearson_1000))
+print("pearson 1,000: " + str(time_pearson_1000))
 print("===================================")
 
 for i in range(num_trials):
@@ -89,21 +89,83 @@ for i in range(num_trials):
 	time_ftest_1000.append(time_elapsed)
 
 print("===================================")
-print("f-test 1000: " + str(time_ftest_1000))
+print("f-test 1,000: " + str(time_ftest_1000))
 print("===================================")
 
-exit(0)
 
 # upload continuous dataset #5000
 subprocess.call(['./upload-client.x',\
 	'/home/sachaservanschreiber/star-spdz/datasets/patient_continuous_5000.txt', \
 	'2','0', '/home/sachaservanschreiber/star-spdz/HOSTS'])
 
-# TODO: perform tests
+for i in range(num_trials):
+	time_start = time.time()
+	subprocess.call(['./client.x', '2', '0', '--ttest', '2', '0', '1', '0', \
+		'/home/sachaservanschreiber/star-spdz/HOSTS'])
+	time_elapsed = (time.time() - time_start)
+	time_ttest_5000.append(time_elapsed)
+
+print("===================================")
+print("t-test 5,000: " + str(time_ttest_5000))
+print("===================================")
+
+for i in range(num_trials):
+	time_start = time.time()
+	subprocess.call(['./client.x', '2', '0', '--pearson', '2', '0', '1', '0', \
+		'/home/sachaservanschreiber/star-spdz/HOSTS'])
+	time_elapsed = (time.time() - time_start)
+	time_pearson_5000.append(time_elapsed)
+
+print("===================================")
+print("pearson 5,000: " + str(time_pearson_5000))
+print("===================================")
+
+for i in range(num_trials):
+	time_start = time.time()
+	subprocess.call(['./client.x', '2', '0', '--ftest', '2', '0', '1', '0', \
+		'/home/sachaservanschreiber/star-spdz/HOSTS'])
+	time_elapsed = (time.time() - time_start)
+	time_ftest_5000.append(time_elapsed)
+
+print("===================================")
+print("f-test 5,000: " + str(time_ftest_5000))
+print("===================================")
+
 
 # upload continuous dataset #10000
 subprocess.call(['./upload-client.x',\
 	'/home/sachaservanschreiber/star-spdz/datasets/patient_continuous_10000.txt', \
 	'2','0', '/home/sachaservanschreiber/star-spdz/HOSTS'])
 
-# TODO: perform tests
+for i in range(num_trials):
+	time_start = time.time()
+	subprocess.call(['./client.x', '2', '0', '--ttest', '2', '0', '1', '0', \
+		'/home/sachaservanschreiber/star-spdz/HOSTS'])
+	time_elapsed = (time.time() - time_start)
+	time_ttest_10000.append(time_elapsed)
+
+print("===================================")
+print("t-test 10,000: " + str(time_ttest_10000))
+print("===================================")
+
+for i in range(num_trials):
+	time_start = time.time()
+	subprocess.call(['./client.x', '2', '0', '--pearson', '2', '0', '1', '0', \
+		'/home/sachaservanschreiber/star-spdz/HOSTS'])
+	time_elapsed = (time.time() - time_start)
+	time_pearson_10000.append(time_elapsed)
+
+print("===================================")
+print("pearson 10,000: " + str(time_pearson_10000))
+print("===================================")
+
+for i in range(num_trials):
+	time_start = time.time()
+	subprocess.call(['./client.x', '2', '0', '--ftest', '2', '0', '1', '0', \
+		'/home/sachaservanschreiber/star-spdz/HOSTS'])
+	time_elapsed = (time.time() - time_start)
+	time_ftest_10000.append(time_elapsed)
+
+print("===================================")
+print("f-test 10,000: " + str(time_ftest_10000))
+print("===================================")
